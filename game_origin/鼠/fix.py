c3aeb85c11e6a0d8d0a2a0c8cdbc69974aacae93
Rochelia <light__and__shadow@outlook.com>
2025-03-02 11:17:54 +0800

    update

--- game_origin/鼠/fix.py
+++
@@ -1,8 +0,0 @@
-name=input()
-cnt=1
-with open(f"C:\\Users\\86150\\PycharmProjects\\PythonProject\\text.txt", 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for line in lines:
-    with open(f"te.txt", 'a', encoding='utf-8') as f:
-        f.write(f"{name}_"+str(cnt)+".mp3|"+line)
-        cnt+=1
